4bc80b6e7d6af7542cb504cc55e0cab1103531ef
yunji111 <2631806735@qq.com>
2020-11-03 01:52:09 +0800

    first commit

---
+++ djangogirls/mysite/trips/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from datetime import datetime
+from .models import Post
+
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post.html', {'post': post})
+
+# Create your views here.
+
+#from django.http import HttpResponse
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+   })
